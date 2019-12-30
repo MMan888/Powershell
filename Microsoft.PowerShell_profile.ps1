ff4b278f8e6e96d7879da8ea27a318221a6f67f1
DevSecOps - London Gathering <42718620+MMan888@users.noreply.github.com>
2019-12-30 09:08:13 +0000

    Added Git functions and general comments

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,15 @@
+# Reference:
 # https://www.red-gate.com/simple-talk/sysadmin/powershell/persistent-powershell-the-powershell-profile/
+
+# Create new profile
+# PS5 and PS6 paths are different
 # New-Item -Path $Profile -Type File -Force
 
+# Sets PS execution policy
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 Write-Host "Execution Policy set to: " (Get-ExecutionPolicy) -ForegroundColor Red
 
+# Console output
 Write-Host "Today:" (Get-Date) -ForegroundColor Green
 Write-Host 'Hostname='"$env:computername" -ForegroundColor Green
 Write-Host "You are logged in as" "$env:username" "using PowerShell"($PSVersionTable.PSVersion.Major) -ForegroundColor Green
@@ -18,9 +24,9 @@ Get-ChildItem
 ######################################
 
 function h10	{Get-History -Count 10}
-function md5    {Get-FileHash -Algorithm MD5 $args }
-function sha1   {Get-FileHash -Algorithm SHA1 $args }
-function sha256 {Get-FileHash -Algorithm SHA256 $args }
+function md5    {Get-FileHash -Algorithm MD5 $args}
+function sha1   {Get-FileHash -Algorithm SHA1 $args}
+function sha256 {Get-FileHash -Algorithm SHA256 $args}
 
 function touch ([string]$Name){
 	New-Item $Name -ItemType file
@@ -46,5 +52,15 @@ function CheckIP{
 # Aliases
 ######################################
 
-Set-Alias ll Get-ChildItem
+Set-Alias -Name list -Value Get-ChildItem
 
+# Get-Alias
+
+######################################
+# Git
+######################################
+
+function ga() {git add -A}
+function gs() {git status}
+function gp() {git pull}
+function gcp($msg) {git commit -m "$msg"; git push}
